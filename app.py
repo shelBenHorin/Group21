de1316a6ae8da260d16be47c6031ff7c9db3e1db
shelBenHorin <shel@post.bgu.ac.il>
2025-01-23 16:03:15 +0200

    python update

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
-# from crypt import methods
-#
-# from flask import Flask
-# from flask import redirect, url_for, render_template
-# app = Flask(__name__)
-# @app.route('/')
-# def home_page():
-#     return 'Hello World!'
-#
-# @app.route('/customers')
-# def customers_page():
-#     return 'Hello customers!'
-#
-# @app.route('/about', methods=['GET','post'])
-# def about_page():
-#     return 'welcome to About!'
-#
-# @app.route('/aboutus')
-# def about5_page():
-#     return redirect('/')
-#
-# @app.route('/login', methods=['GET','post'])
-# def login_page():
-#     return render_template('login.html')
+
+from crypt import methods
+import pymongo
+
+from flask import Flask
+from flask import redirect, url_for, render_template
+
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://ShelAlma:ShelAlma@cluster0.3whqy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+app = Flask(__name__)
+
