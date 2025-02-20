b29a974e8021c82b44a375ef4978f2c8b9da1b74
AlmaBickel1 <almab@post.bgu.ac.il>
2025-02-20 10:41:40 +0200

    updated

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)
-from db_connector import mydatabase
+from flask import Flask, session, redirect, url_for, render_template
+import os
+
+template_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'pages', 'feed', 'templates')
+
+app = Flask(__name__, template_folder=template_dir, static_folder='pages/feed/static')
+
 @app.route('/')
-def homepage():
+def feed():
     return render_template('feed.html')
 
+if __name__ == '__main__':
+    app.run(debug=True)
 # # Send a ping to confirm a successful connection
 # try:
 #     client.admin.command('ping')
